3c84e951d346a1db54bd496c2df5c594dc77fb8e
Yevhen Bilokin <zenyen14@gmail.com>
2024-12-28 12:07:22 +0100

    Test Telegram API integration

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
+import telebot
 
+from config import BOT_TOKEN
+
+bot = telebot.TeleBot(BOT_TOKEN, parse_mode=None)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Hello!")
+
+
+bot.infinity_polling()
