0e8a6d30223f2c341a9284cf755c989e357f9915
Yevhen Bilokin <zenyen14@gmail.com>
2024-12-29 22:35:24 +0100

    Add functions to manage ueser data and database in db_mananger.py

---
+++ app/utils/db_manager.py
@@ -0,0 +1,46 @@
+import sqlite3
+from app.config import DB_PATH
+
+#Create and check database if it exists 
+def create_table():
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY,  
+        gender TEXT,                  
+        weight REAL                   
+    )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+def save_useer_data(user_id, gender, weight):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    
+    cursor.execute('''
+    INSERT INTO users (user_id, gender, weight) 
+    VALUES (?, ?, ?)
+    ON CONFLICT(user_id) 
+    DO UPDATE SET gender = ?, weight = ?
+    ''', (user_id, gender, weight, gender, weight))
+    
+    conn.commit()
+    conn.close()
+
+def get_user_data(user_id):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    
+    cursor.execute("SELECT * FROM users WHERE user_id = ?", (user_id,))
+    user_data = cursor.fetchone()
+    conn.close()
+    
+    return user_data
+
+
+if __name__ == "__main__":
+    create_table()
